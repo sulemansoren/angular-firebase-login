ea76d99e1e52e2b0f3225128a636f6251a7350f6
Suleman Soren <sulemansoren@Sulemans-MacBook-Pro.local>
2020-07-31 16:22:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loginapp';
+
+  constructor(public auth: AngularFireAuth) {
+  }
+  login() {
+    this.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.auth.signOut();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBDLZXDBqe5LhtnhjieoQzTEWqhr_dmEA",
+  authDomain: "abide-assignment.firebaseapp.com",
+  databaseURL: "https://abide-assignment.firebaseio.com",
+  projectId: "abide-assignment",
+  storageBucket: "abide-assignment.appspot.com",
+  messagingSenderId: "109996230141",
+  appId: "1:109996230141:web:3831be2ed17ea5f0f1c594",
+  measurementId: "G-J6LMEXVC6Q"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
